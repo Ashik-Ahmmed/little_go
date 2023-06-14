981c28937e9787f73955a3b9f56f282f9160a1cc
Ashik Ahmmed <ashikahmmedshuvo@gmail.com>
2023-06-14 14:38:57 +0600

    function return multiple

--- chapter1/main.go
+++ chapter1/main.go
@@ -1,7 +1,16 @@
 package main
 
+import "fmt"
+
+func getPower() int {
+	return 9000
+}
+func nameAndPower() (string, int) {
+	return "Ashik", 9000
+}
 func main() {
-	var power int
-	power = 9000
-	println("It is over", power)
+	//var power int = 9000
+	name, _ := nameAndPower()
+	_, age := nameAndPower()
+	fmt.Printf("It is %s over %d \n", name, age)
 }
