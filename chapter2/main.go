168cbc4f0f5022886815e150ab7a390f1ed8911a
Ashik Ahmmed <ashikahmmedshuvo@gmail.com>
2023-06-14 17:08:53 +0600

    structure,poinbter,composition

--- chapter2/main.go
+++ chapter2/main.go
@@ -2,6 +2,42 @@ package main
 
 import "fmt"
 
+//composing user with person
+type Person struct {
+	Name string
+}
+
+func (person *Person) introduce() {
+	fmt.Printf("My name is %s", person.Name)
+}
+
+type User struct {
+	*Person
+	Age    int
+	Father *User
+}
+
+func (user *User) increaseAge() {
+	user.Age += 10
+}
+
+//factory function to create instance of user
+func userFactory(name string, age int, father *User) *User {
+	return &User{
+		Person: &Person{name},
+		Age:    age,
+		Father: father,
+	}
+}
+
 func main() {
-	fmt.Printf("hello Chap 2\n")
+	user1 := userFactory("Ashik", 27, nil)
+	user1.increaseAge()
+	user1.increaseAge()
+	user1.introduce()
+	user2 := userFactory("child", 13, user1)
+	fmt.Printf("name and age of user %s,%d \n", user1.Name, user1.Age)
+	fmt.Printf("name and age of user %s,%d \n", user2.Name, user2.Age)
+	fmt.Printf("Father name %s and age %d", user2.Father.Name, user2.Father.Age)
+
 }
